d52765ca26f1a9b4f4c62eb13f0b93df7261a8fa
YangXiaos <178069857@qq.com>
2018-05-26 22:09:55 +0800

    站点配置Dao层次

---
+++ DioFramework/Dao/SiteConfigDao.py
@@ -0,0 +1,31 @@
+# @Time         : 18-5-26 下午8:01
+# @Author       : DioMryang
+# @File         : SiteConfigDao.py
+# @Description  :
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+from DioCore.DB import MysqlUnit
+
+from DioFramework import Const
+
+
+class SiteConfig(declarative_base()):
+
+    __tablename__ = 'dio.site.config'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    desc = Column(String)
+    domain = Column(String)
+    parend_id = Column(Integer)
+    tag = Column(String)
+    status = Column(Integer, default=0)
+
+
+if __name__ == '__main__':
+    conn = MysqlUnit.createConnect(**Const.MYSQL_CONFIG)
+
+    site = SiteConfig(desc="baidu - 导航页", domain="baidu.com", parend_id=None, tag=0, status=0)
+    conn.add(site)
+    conn.commit()
+    conn.close()
---
+++ DioFramework/Dao/__init__.py
@@ -0,0 +1,4 @@
+# @Time         : 18-5-17 下午10:31
+# @Author       : DioMryang
+# @File         : __init__.py.py
+# @Description  : 
